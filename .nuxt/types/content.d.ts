10a4d7b0757d6e45105adb056f6853f19e4cc2fe
Youness Ait Ali <younes.aitali95@gmail.com>
2022-06-15 00:01:01 +0100

    chore: Delete .nuxt Folder

--- .nuxt/types/content.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '#content/server' {
-  const serverQueryContent: typeof import('/home/youness/dev/portfolio-2.0/node_modules/.pnpm/@nuxt+content@2.0.1/node_modules/@nuxt/content/dist/runtime/server').serverQueryContent
-  const parseContent: typeof import('/home/youness/dev/portfolio-2.0/node_modules/.pnpm/@nuxt+content@2.0.1/node_modules/@nuxt/content/dist/runtime/server').parseContent
-}
