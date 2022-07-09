5676836572ecf2c49a9676fd447c5c1def451c81
younessaitali <younes.aitali95@gmail.com>
2022-07-09 02:34:34 +0100

    fix: bio nav rotation issue

--- components/BioNav.vue
+++ components/BioNav.vue
@@ -8,7 +8,7 @@
         xl="-right-16  "
         lg="w-80 -right-28 bottom-1/2 inset-x-auto rotate-90"
         md="inset-x-auto left-28 right-0  bottom-28  "
-        class="px-10 w-58 fixed z-50 inset-x-0 mx-auto bottom-5">
+        class="px-10 w-58 rotate-180 fixed z-50 inset-x-0 mx-auto bottom-5">
         <ul class="items-center grid gap-x-6 grid-cols-5">
             <li
                 class="h-5 w-5 border-2 flex items-center justify-center border-darkBlue dark:border-champagne rounded-md"
