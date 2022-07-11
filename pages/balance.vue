ea72577fc8e202c4b76547b8d365415c66f7f88f
younessaitali <younes.aitali95@gmail.com>
2022-07-11 16:04:25 +0100

    feat: add text for balance page +change title on support page

--- pages/balance.vue
+++ pages/balance.vue
@@ -13,20 +13,24 @@
             class="w-full min-h-full flex flex-col items-center justify-center">
             <div
                 class="flex flex-col items-center my-auto xl:max-h-2xl lg:max-h-2xl md:max-h-xl gap-y-3">
-                <head-line title="Like to have fun" />
-                <!-- md:h-82 2xl:h-lg xl:h-md -->
+                <head-line title="Trying his very best to be balanced" />
                 <BalanceAnimation class="2xl:max-h-sm max-h-xs" :key="uuid" />
-                <p
+
+                <figure
                     font="medium"
-                    class="mx-a px-2 xl:max-w-3xl lg:max-w-2xl"
+                    class="mx-a relative xl:max-w-3xl lg:max-w-2xl"
                     text="champagne center lg:xl md">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Donec euismod, nisi eu consectetur consectetur, Lorem ipsum
-                    dolor sit amet, consectetur adipiscing elit. Donec euismod,
-                    nisi eu consectetur consectetur,oLorem ipsum dolor sit amet,
-                    consectetur adipiscing elit. Donec euismod, nisi eu
-                    consectetur consectetur,
-                </p>
+                    <blockquote class="italic mt-4 font-display font-bold mb-4">
+                        <i class="text-4xl">“</i>
+                        Too much of one thing can end up creating stress. this
+                        is something that no one needs in their life. But living
+                        a life in balance can provide harmony and peace.
+                        <i class="text-4xl">”</i>
+                    </blockquote>
+                    <cite class="font-medium underline"
+                        >Catherine Pulsifer.</cite
+                    >
+                </figure>
             </div>
         </div>
     </nuxt-layout>
